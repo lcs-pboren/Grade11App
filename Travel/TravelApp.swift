8b0c2efe66a314d898dfad2ae6ec988aaadda011
Patrick Boren <Pboren@lcs.on.ca>
2021-11-29 14:42:58 -0500

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Patrick Boren on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
